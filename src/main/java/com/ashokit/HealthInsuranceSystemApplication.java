0e7fbd9761791b4f9e250a0dcde4ebe1f37c8e97
DEV <DEV@gmail.com>
2020-09-21 22:29:41 +0530

    main Class changed

--- src/main/java/com/ashokit/HealthInsuranceSystemApplication.java
+++ src/main/java/com/ashokit/HealthInsuranceSystemApplication.java
@@ -2,12 +2,17 @@ package com.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HealthInsuranceSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HealthInsuranceSystemApplication.class, args);
+		System.out.println("Main start");
+		ConfigurableApplicationContext run = SpringApplication.run(HealthInsuranceSystemApplication.class, args);
+		System.out.println("Main End");
+		System.out.println("Closing run");
+		run.close();
 	}
 
 }
